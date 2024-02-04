d381ad89fd7f23a6090a61467da899ffc074819b
Yusuf Bachtiar <yusufbachtiar@DOKUs-MacBook-Pro-2.local>
2024-02-04 16:46:16 +0700

    feat:change class to className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <header>
-      <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400 mt-8">© 2024 Yusuf Bachtiar. DevscaleID Batch 4</span>
+      <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400 mt-8">© 2024 Yusuf Bachtiar. DevscaleID Batch 4</span>
     </header>
   )
 }
